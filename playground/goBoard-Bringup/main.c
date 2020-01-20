e7bf305decc9a7ac437dc67c665103e9c2d0aaf9
Paul Schneider <paul@konyn.us>
2020-01-19 18:17:35 -0800

    In progress, CAN0

---
+++ playground/goBoard-Bringup/main.c
@@ -0,0 +1,296 @@
+// goBoard Bringup Development
+// Author: Paul Schneider
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "inc/tm4c123fh6pm.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_can.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/can.h"
+
+void delay_ms(int milliseconds) {
+    SysCtlDelay(SysCtlClockGet() / 3 * milliseconds / 1000);
+}
+void delay_sec(int seconds) {
+    SysCtlDelay(SysCtlClockGet() / 3 * seconds);
+}
+
+
+
+volatile uint32_t g_ui32MsgCount = 0;
+volatile bool g_bErrFlag = 0;
+
+void CANIntHandler(void) {
+    uint32_t ui32Status;
+
+    //
+    // Read the CAN interrupt status to find the cause of the interrupt
+    //
+    ui32Status = CANIntStatus(CAN0_BASE, CAN_INT_STS_CAUSE);
+
+    //
+    // If the cause is a controller status interrupt, then get the status
+    //
+    if(ui32Status == CAN_INT_INTID_STATUS)
+    {
+        //
+        // Read the controller status.  This will return a field of status
+        // error bits that can indicate various errors.  Error processing
+        // is not done in this example for simplicity.  Refer to the
+        // API documentation for details about the error status bits.
+        // The act of reading this status will clear the interrupt.  If the
+        // CAN peripheral is not connected to a CAN bus with other CAN devices
+        // present, then errors will occur and will be indicated in the
+        // controller status.
+        //
+        ui32Status = CANStatusGet(CAN0_BASE, CAN_STS_CONTROL);
+
+        //
+        // Set a flag to indicate some errors may have occurred.
+        //
+        g_bErrFlag = 1;
+    }
+
+    //
+    // Check if the cause is message object 1, which what we are using for
+    // sending messages.
+    //
+    else if(ui32Status == 1)
+    {
+        //
+        // Getting to this point means that the TX interrupt occurred on
+        // message object 1, and the message TX is complete.  Clear the
+        // message object interrupt.
+        //
+        CANIntClear(CAN0_BASE, 1);
+
+        //
+        // Increment a counter to keep track of how many messages have been
+        // sent.  In a real application this could be used to set flags to
+        // indicate when a message is sent.
+        //
+        g_ui32MsgCount++;
+
+        //
+        // Since the message was sent, clear any error flags.
+        //
+        g_bErrFlag = 0;
+    }
+
+    //
+    // Otherwise, something unexpected caused the interrupt.  This should
+    // never happen.
+    //
+    else
+    {
+        //
+        // Spurious interrupt handling can go here.
+        //
+    }
+}
+
+void InitCAN() {
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    GPIOPinConfigure(GPIO_PB4_CAN0RX);
+    GPIOPinConfigure(GPIO_PB5_CAN0TX);
+    GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
+    CANInit(CAN0_BASE);
+    CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 500000);
+    CANIntRegister(CAN0_BASE, CANIntHandler); // use dynamic vector table allocation
+    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
+    IntEnable(INT_CAN0);
+    CANEnable(CAN0_BASE);
+}
+
+int main(void)
+{
+    puts("Hello!");
+    printf("goBoard Boot!\n");
+    printf("Boot SysClock = %d\n", SysCtlClockGet());
+
+    if (IntMasterEnable()) {
+        printf("Interrupts enabled!\n");
+    }
+    else {
+        printf("Interrupts were already enabled!\n");
+    }
+
+
+    // Sys clock ???
+    SysCtlClockSet(SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ | SYSCTL_SYSDIV_8 | SYSCTL_USE_PLL);
+
+    printf("Mod SysClock = %d\n", SysCtlClockGet());
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+
+//    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+//    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+//    GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5, 0xFF);
+
+    //while(1);
+
+    //InitCAN();
+
+    tCANMsgObject sCANMessage;
+    uint32_t ui32MsgData;
+    uint8_t *pui8MsgData;
+
+    pui8MsgData = (uint8_t *)&ui32MsgData;
+
+    //
+    // For this example CAN0 is used with RX and TX pins on port B4 and B5.
+    // The actual port and pins used may be different on your part, consult
+    // the data sheet for more information.
+    // GPIO port B needs to be enabled so these pins can be used.
+    // TODO: change this to whichever GPIO port you are using
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);///////////////////////////////////////////////////changed
+
+    //
+    // Configure the GPIO pin muxing to select CAN0 functions for these pins.
+    // This step selects which alternate function is available for these pins.
+    // This is necessary if your part supports GPIO pin function muxing.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using
+    //
+    GPIOPinConfigure(GPIO_PB4_CAN0RX);///////////////////////////////////////////////////////////changed
+    GPIOPinConfigure(GPIO_PB5_CAN0TX);
+
+    //
+    // Enable the alternate function on the GPIO pins.  The above step selects
+    // which alternate function is available.  This step actually enables the
+    // alternate function instead of GPIO for these pins.
+    // TODO: change this to match the port/pin you are using
+    //
+    GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);//////////////////////////////////changed
+
+    //
+    // The GPIO port and pins have been set up for CAN.  The CAN peripheral
+    // must be enabled.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
+
+    //
+    // Initialize the CAN controller
+    //
+    CANInit(CAN0_BASE);
+
+    //
+    // Set up the bit rate for the CAN bus.  This function sets up the CAN
+    // bus timing for a nominal configuration.  You can achieve more control
+    // over the CAN bus timing by using the function CANBitTimingSet() instead
+    // of this one, if needed.
+    // In this example, the CAN bus is set to 500 kHz.  In the function below,
+    // the call to SysCtlClockGet() or ui32SysClock is used to determine the
+    // clock rate that is used for clocking the CAN peripheral.  This can be
+    // replaced with a  fixed value if you know the value of the system clock,
+    // saving the extra function call.  For some parts, the CAN peripheral is
+    // clocked by a fixed 8 MHz regardless of the system clock in which case
+    // the call to SysCtlClockGet() or ui32SysClock should be replaced with
+    // 8000000.  Consult the data sheet for more information about CAN
+    // peripheral clocking.
+    //
+#if defined(TARGET_IS_TM4C129_RA0) ||                                         \
+    defined(TARGET_IS_TM4C129_RA1) ||                                         \
+    defined(TARGET_IS_TM4C129_RA2)
+    CANBitRateSet(CAN0_BASE, ui32SysClock, 500000);
+#else
+    CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 500000);/////////////////////////////////////////////////////////////CAN speed???
+#endif
+
+    //
+    // Enable interrupts on the CAN peripheral.  This example uses static
+    // allocation of interrupt handlers which means the name of the handler
+    // is in the vector table of startup code.  If you want to use dynamic
+    // allocation of the vector table, then you must also call CANIntRegister()
+    // here.
+    //
+    // CANIntRegister(CAN0_BASE, CANIntHandler); // if using dynamic vectors
+    //
+    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
+
+    //
+    // Enable the CAN interrupt on the processor (NVIC).
+    //
+    IntEnable(INT_CAN0);
+
+    //
+    // Enable the CAN for operation.
+    //
+    CANEnable(CAN0_BASE);
+
+    ui32MsgData = 0;
+    sCANMessage.ui32MsgID = 1;
+    sCANMessage.ui32MsgIDMask = 0;
+    sCANMessage.ui32Flags = MSG_OBJ_TX_INT_ENABLE;
+    sCANMessage.ui32MsgLen = sizeof(pui8MsgData);
+    sCANMessage.pui8MsgData = pui8MsgData;
+
+
+    while (1) {
+        printf("tick!\n");
+        //
+        // Print a message to the console showing the message count and the
+        // contents of the message being sent.
+        //
+        printf("Sending msg: 0x%02X %02X %02X %02X\n",
+                   pui8MsgData[0], pui8MsgData[1], pui8MsgData[2],
+                   pui8MsgData[3]);
+
+        //
+        // Send the CAN message using object number 1 (not the same thing as
+        // CAN ID, which is also 1 in this example).  This function will cause
+        // the message to be transmitted right away.
+        //
+        CANMessageSet(CAN0_BASE, 1, &sCANMessage, MSG_OBJ_TYPE_TX);
+
+        //
+        // Now wait 1 second before continuing
+        //
+        delay_ms(100);
+
+        //
+        // Check the error flag to see if errors occurred
+        //
+        if(g_bErrFlag)
+        {
+            printf(" error - cable connected?\n");
+        }
+        else
+        {
+            //
+            // If no errors then print the count of message sent
+            //
+//            int tmp = 0;
+//            for (int i = 0; i < 3; i++) {
+//                tmp += i;
+//            }
+            printf(" total count = %u\n", g_ui32MsgCount);
+        }
+
+        //
+        // Increment the value in the message data.
+        //
+        ui32MsgData++;
+
+        delay_ms(1500);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0xFF);
+        delay_ms(1500);
+        GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x00);
+    }
+
+
+}
